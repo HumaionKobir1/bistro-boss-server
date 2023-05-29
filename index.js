172184ca21eb194acc43fbaafff82d75ad1f7479
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-29 16:54:53 +0600

    careate a jwt token and save it on client

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+var jwt = require('jsonwebtoken');
+
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -34,10 +36,51 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection('reviews');
     const cartCollection = client.db("bistroDb").collection('carts');
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+
+      res.send({token});
+    })
+
+
+
     // users related api
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post('/users', async(req, res) => {
       const user = req.body;
+      console.log(user);
+
+      const query = {email: user.email};
+
+      const existingUser = await usersCollection.findOne(query);
+      console.log('existing user:', existingUser)
+
+      if(existingUser) {
+        return res.send({message: 'user already exists'});
+      }
+
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
 
