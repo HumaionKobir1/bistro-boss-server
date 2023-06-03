e1c4000583db230010ff4c0e1484444eb447ffea
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-06-03 14:28:38 +0600

    payment intent api to get client secret from strib

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var jwt = require('jsonwebtoken');
 const Stripe = require('stripe');
-const stripe = Stripe('sk_test_51NETPuB9B9Ycv5F96oMYC9MaZuGESrSUo0ZqDML87S8EmvALvx3fceh4RLKkWcgizQYHASnkhC8noWxHaaxDq9kp00Sr4nREaU');
+const stripe = Stripe('');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
